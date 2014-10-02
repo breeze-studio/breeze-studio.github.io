0c6a9cf2f785f5d8e13e83b8a3ca7e78e8e57e9e
breeze-studio <breeze-studio@users.noreply.github.com>
2014-10-02 23:04:46 +0700

    Загружены файлы сайта.

---
+++ script.js
@@ -0,0 +1,55 @@
+﻿(function(breeze, $)
+{
+	var _expandButtonText = "Подробнее…";
+	var _collapseButtonText = "Скрыть подробности";
+
+	function start()
+	{
+		windowScroll();
+		$(window).scroll(windowScroll);
+		$(".collapsed").each(crateCollapsed);
+	}
+
+	function windowScroll()
+	{
+		if($(window).scrollTop() > 85)
+			$("header").addClass("fixed");
+		else
+			$("header").removeClass("fixed");
+	}
+
+	function crateCollapsed()
+	{
+		var item = $(this);
+		var customText = item.attr("data-text");
+		var expandButtonText = customText || _expandButtonText;
+		var collapseButtonText = customText || _collapseButtonText;
+		var button = $("<p class='expandButton'>").text(expandButtonText);
+		item.before(
+			button.click(function()
+			{
+				if(!item.is(":visible")) button.text(collapseButtonText);
+				item.slideToggle(function()
+				{
+					if(!item.is(":visible")) button.text(expandButtonText);
+				});
+			}));
+	}
+
+	if(!String.prototype.fmt)
+	{
+		String.prototype.fmt = function()
+		{
+			var args = arguments;
+			return this.replace(/{(\d+)}/g, function(match, number)
+			{
+				return typeof args[number] != "undefined" ? args[number] : match;
+			});
+		};
+	}
+
+	breeze.start = start;
+
+})(window.breeze = window.breeze || {}, $);
+
+$(breeze.start);
